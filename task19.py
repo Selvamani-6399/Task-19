c72e367a250e9ad214cc97d2bbb64c320c85cfed
Selvamani-6399 <150180371+Selvamani-6399@users.noreply.github.com>
2024-02-13 10:06:13 -0700

    I have uploaded my task -19 code

---
+++ task19.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+class LoginPage:
+
+    def __init__(self, url):
+        self.url = url
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    def boot(self):
+        self.driver.get(self.url)
+        self.driver.maximize_window()
+        sleep(5)
+
+    def quit(self):
+        self.driver.quit()
+
+    def login(self):
+
+        username_input = self.driver.find_element(by=By.NAME, value="user-name")   
+        password_input = self.driver.find_element(by=By.NAME, value="password")
+
+        username_input.send_keys("standard_user")           
+        password_input.send_keys("secret_sauce")
+
+        self.driver.find_element(by=By.TAG_NAME, value="button").click()
+        
+        sleep(5)
+
+    def getTitle(self):
+        return self.driver.title    
+
+    def getURL(self):
+        return self.driver.current_url
+
+    def sourceCode(self):
+        return self.driver.page_source
+
+
+
+url = "https://www.saucedemo.com/inventory.html"
+obj = LoginPage(url)
+obj.boot()
+obj.login()
+print(obj.getURL())
+print(obj.getTitle())
+print(obj.sourceCode())     
+obj.quit()  
